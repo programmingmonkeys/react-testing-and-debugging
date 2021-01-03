82b97bc6ef2bb167059eb3777a415c7745e70d95
codingChewie <codingChewie@gmail.com>
2021-01-03 15:14:57 -0600

    organize your tests with describe

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -2,38 +2,43 @@ import data from "../../data/courses.json";
 
 const numItems = data.length;
 
-test("Number of items = 12", () => {
-  expect(numItems).toBe(12);
-});
+describe("Number test", () => {
+  test("Number of items = 12", () => {
+    expect(numItems).toBe(12);
+  });
 
-test("Number of items to be greater than or equal to 12", () => {
-  expect(numItems).toBeGreaterThanOrEqual(12);
+  test("Number of items to be greater than or equal to 12", () => {
+    expect(numItems).toBeGreaterThanOrEqual(12);
+  });
 });
 
 const dataTest = data[0].title;
 
-test("There is a JS in this title", () => {
-  expect(dataTest).toMatch(/JS/);
+describe("String tests", () => {
+  test("There is a JS in this title", () => {
+    expect(dataTest).toMatch(/JS/);
+  });
+
+  test("The title contains React", () => {
+    expect(dataTest).toContain("React");
+  });
 });
 
-test("The title contains React", () => {
-  expect(dataTest).toContain("React");
-});
-
-// Arrays
+// Arrays & Objects
 const data2 = ["React Native", "React"];
 
-test("The list of courses contains React Native and React", () => {
-  expect(["React Native", "React", "MeteorJS"]).toEqual(
-    expect.arrayContaining(data2)
-  );
-});
+describe("Arrays and Objects tests", () => {
+  test("The list of courses contains React Native and React", () => {
+    expect(["React Native", "React", "MeteorJS"]).toEqual(
+      expect.arrayContaining(data2)
+    );
+  });
 
-// Objects
-test("The first course to have a property title", () => {
-  expect(data[0]).toHaveProperty("title");
-});
+  test("The first course to have a property title", () => {
+    expect(data[0]).toHaveProperty("title");
+  });
 
-test("The first course to have a property title and value of 31,266", () => {
-  expect(data[0]).toHaveProperty("views", "31,266");
+  test("The first course to have a property title and value of 31,266", () => {
+    expect(data[0]).toHaveProperty("views", "31,266");
+  });
 });
